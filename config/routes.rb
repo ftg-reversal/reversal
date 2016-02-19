1a4da54196db0830242fb71d9a6eca861f1b1c9e
tigberd <tigberd@gmail.com>
2016-02-19 21:38:25 +0900

    Routeの整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
-# == Route Map
-#
-# Prefix Verb URI Pattern Controller#Action
-#   root GET  /           videos#index
-#
-
 Rails.application.routes.draw do
   root to: 'top#index'
-
-  get '/videos', to: 'videos#index'
-
-  get '/slack', to: 'slack#index'
-  get '/slack/show/:channel', to: 'slack#show'
 
   get '/auth/:provider/callback', to: 'sessions#create'
   resource :session, only: [:destroy]
 
-  # get '@:name', to: 'slack#users', as: 'user'
+  resources :videos, only: [:index]
+  resources :slack_channels, as: :channels, path: :channels, only: [:index, :show]
+  resources :summaries
+
+  namespace :api, { format: 'json' } do
+    resources :channels, as: :channels, path: :channels, only: [:index, :show]
+  end
+
+  get '@:name', to: 'slack_channels#users', as: 'user'
   get 'image', to: 'proxies#show', as: 'image_proxy'
 end
