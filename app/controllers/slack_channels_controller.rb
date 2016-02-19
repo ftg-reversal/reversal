0edf817e25b0f61f04cb4c4fb5ec61cb25a60135
tigberd <tigberd@gmail.com>
2016-02-19 21:37:54 +0900

    コントローラー名の変更

--- app/controllers/slack_channels_controller.rb
+++ app/controllers/slack_channels_controller.rb
@@ -1,11 +1,11 @@
-class SlackController < ApplicationController
+class SlackChannelsController < ApplicationController
   before_action :set_channels
 
   def index
   end
 
   def show
-    @channel = SlackChannel.find_by(name: params[:channel])
+    @channel = SlackChannel.find_by(id: params[:id])
     @history = @channel.slack_message.decorate
   end
 
