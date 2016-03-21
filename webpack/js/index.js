1e40316cfa0f8f6fbbcf0b2a27215345cbda4902
tigberd <tigberd@gmail.com>
2016-03-21 22:02:10 +0900

    まとめでのツイートの展開に対応

--- webpack/js/index.js
+++ webpack/js/index.js
@@ -1,10 +1,12 @@
-require('jquery');
-require('jquery-ujs');
-require('sticky-kit');
-require('jQuery.mmenu');
+require("jquery");
+require("jquery-ujs");
+require("sticky-kit");
+require("jQuery.mmenu");
 
-require('uikit');
+require("uikit");
 require("font-awesome-webpack");
+
+require("./twitter/widgets.js");
 
 require("./views/slack");
 
