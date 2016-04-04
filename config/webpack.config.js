3250988b6a27c3032fe1a6a3f79c0b6a482aa1d5
tigberd <tigberd@gmail.com>
2016-04-04 17:02:41 +0900

    otfファイルに対応

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -48,7 +48,7 @@ var config = {
       { test: /\.png$/, loader: "url?mimetype=image/png" },
 
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+      { test: /\.(otf|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
     ]
   },
   postcss: function () {
