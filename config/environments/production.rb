e60d15ce3ad22b32d9c77c191542c9f9fa62dac9
tigberd <tigberd@gmail.com>
2016-04-09 20:44:44 +0900

    CKEditorのために色々調整

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,13 +15,14 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
 
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.active_record.raise_in_transactional_callbacks = true
   config.paperclip_defaults = {
     storage: :s3,
     bucket: ENV['S3_BUCKET_NAME'],
     s3_region: ENV['S3_REGION'],
-    s3_host_name: ENV['S3_HOST_NAME'],
-    path: "/:class/:id/:style/:clean_filename",
-    url:  "/:class/:id/:style/:clean_filename",
+    path: "/:class/:id/:style/:filename",
+    url:  "/:class/:id/:style/:filename",
     s3_credentials: {
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
