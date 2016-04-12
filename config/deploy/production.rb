fd16ab5629e7eddc1f88905b638fe184792e5d22
tigberd <tigberd@gmail.com>
2016-04-12 17:57:00 +0900

    unicorn_rack_envにproductionを指定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,6 +4,12 @@ set :deploy_to, "/home/#{fetch(:user)}/apps/reversal"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
+# capistrano3-unicorn.rbの中で
+# set :unicorn_rack_env, -> { fetch(:rails_env) == "development" ? "development" : "deployment" }
+# execute :bundle, "exec unicorn", "-c", fetch(:unicorn_config_path), "-E", fetch(:unicorn_rack_env), "-D", fetch(:unicorn_options)
+# されてるから仕方なく指定、ちょっと辛い
+set :unicorn_rack_env, 'production'
+
 server 'ftg-reversal.net',
   roles: [:web, :app],
   port: fetch(:port),
