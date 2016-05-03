a847664e135f7dd0581c51071594537fd85a4fea
tigberd <tigberd@gmail.com>
2016-05-03 18:28:05 +0900

    emojiを3分おきにSlackApiに取りにいくように修正

--- app/infrastructures/slack_infrastructure/emoji.rb
+++ app/infrastructures/slack_infrastructure/emoji.rb
@@ -2,13 +2,13 @@ module SlackInfrastructure
   class Emoji
     class << self
       # @return [Array<Hash>]
-      def exec
-        client = SlackInfrastructure::SlackClient.exec
-        @list ||= client.emoji_list['emoji'].tap do |list|
-          list.map do |key, value|
-            if value =~ /\Aalias:(.+)\z/
-              list[key] = list[$1]
-            end
+      def exec(cache = true)
+        Rails.cache.delete('slack_emoji_api') unless cache
+
+        Rails.cache.fetch('slack_emoji_api', expires_in: 3.minutes) do
+          client = SlackInfrastructure::SlackClient.exec
+          client.emoji_list['emoji'].tap do |list|
+            list.map { |key, value| list[key] = list[$1] if value =~ /\Aalias:(.+)\z/ }
           end
         end
       end
