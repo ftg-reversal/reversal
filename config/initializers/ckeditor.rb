3ff0f30b2dd9c636de613078113c4580053de4ba
tigberd <tigberd@gmail.com>
2016-05-06 02:11:03 +0900

    ckeditorのassets設定を追加

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -2,4 +2,5 @@
 Ckeditor.setup do |config|
   require "ckeditor/orm/active_record"
   config.assets_languages = ['ja', 'en']
+  config.assets_plugins = ['autoembed', 'autolink', 'embed', 'embedbase', 'image2', 'imageresponsive', 'lineutils', 'notification', 'notificationaggregator', 'simpleuploads', 'widget']
 end
