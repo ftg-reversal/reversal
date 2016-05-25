bc7cfc606bbc55e7545ca637d9693050a37d13dd
yuki_yano <yuki_yano@dwango.co.jp>
2016-05-25 19:24:15 +0900

    URIエンコードの処理を追加

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -1,7 +1,7 @@
 module TwitterHelper
   # rubocop:disable Metrics/LineLength
   def tw(url, text)
-    content_tag(:a, 'tweet', href: "https://twitter.com/intent/tweet?button_hashtag=ftg_reversal&text=#{text}", class: 'twitter-hashtag-button', data: { size: 'large', url: url })
+    content_tag(:a, 'tweet', href: "https://twitter.com/intent/tweet?button_hashtag=ftg_reversal&text=#{url_encode(text)}", class: 'twitter-hashtag-button', data: { size: 'large', url: url })
   end
   # rubocop:enable Metrics/LineLength
 end
