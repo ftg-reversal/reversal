3d936ba4e11ad75d5c71cb19b03c4317c3c79dee
yuki_yano <yuki_yano@dwango.co.jp>
2016-05-27 19:32:21 +0900

    トップの動画の順序を投稿日時順に修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
-    @videos = Video.order('updated_at DESC').limit(3)
+    @videos = Video.order('posted_at DESC').limit(3)
     @pages = Page.order('updated_at DESC').limit(3)
     @summaries = Summary.order('updated_at DESC').limit(3)
     @events = Event.order('updated_at DESC').limit(3)
