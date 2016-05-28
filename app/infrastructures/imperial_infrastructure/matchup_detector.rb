3f5a1c7af01e862136d74270339d5c65db1404dd
tigberd <tigberd@gmail.com>
2016-05-29 02:17:49 +0900

    imperialの閾値を適切に修正

--- app/infrastructures/imperial_infrastructure/matchup_detector.rb
+++ app/infrastructures/imperial_infrastructure/matchup_detector.rb
@@ -4,7 +4,7 @@ module ImperialInfrastructure
   class MatchupDetector
     class << self
       def exec(video_id)
-        string_io = open("http://imperial/100/80/#{video_id}", read_timeout: 2000)
+        string_io = open("http://imperial/80/100/#{video_id}", read_timeout: 2000)
         JSON.parse(string_io.read)
       rescue
         {}
