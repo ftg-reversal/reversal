882094e525f65afe36b2f519892a577efa2db710
yuki_yano <yuki_yano@dwango.co.jp>
2016-05-30 12:13:34 +0900

    nprogress-railsを削除

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,5 @@
 //= require ckeditor/init
 //= require jquery
-//= require nprogress
-//= require nprogress-turbolinks
 //= require jquery.datetimepicker
 //= require jquery.datetimepicker/init
 //= require_tree ./webpack
