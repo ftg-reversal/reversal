b05f96156829e833da32a09903f3f294e8c41e2e
tigberd <tigberd@gmail.com>
2016-05-30 10:05:22 +0900

    Google Analyticsの導入

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,11 +21,13 @@ Rails.application.configure do
     storage: :s3,
     bucket: ENV['S3_BUCKET_NAME'],
     s3_region: ENV['S3_REGION'],
-    path: "/:class/:id/:style/:filename",
+    path: '/:class/:id/:style/:filename',
     url: ':s3_domain_url',
     s3_credentials: {
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
+  GA.tracker = ENV['GOOGLE_ANALYTICS']
 end
