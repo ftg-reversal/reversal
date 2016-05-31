25190577c008020819f9428e3296be51c471c822
yuki_yano <yuki_yano@dwango.co.jp>
2016-05-31 15:09:12 +0900

    一時的にImperialのタイムアウト時間を2倍に変更

--- app/infrastructures/imperial_infrastructure/matchup_detector.rb
+++ app/infrastructures/imperial_infrastructure/matchup_detector.rb
@@ -4,7 +4,7 @@ module ImperialInfrastructure
   class MatchupDetector
     class << self
       def exec(video_id)
-        string_io = open("http://imperial/80/100/#{video_id}", read_timeout: 2000)
+        string_io = open("http://imperial/80/100/#{video_id}", read_timeout: 4000)
         JSON.parse(string_io.read)
       rescue
         {}
