2aaa0da9589af8d6981ba022badee8715fd868b1
yuki_yano <yuki_yano@dwango.co.jp>
2016-06-02 21:50:42 +0900

    ログイン後にリダイレクトする処理の実装

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,4 +1,8 @@
 module SessionHelper
+  def login_with_redirect_url(redirect_to)
+    "/login?redirect_to=#{redirect_to}"
+  end
+
   def auth_url(service)
     if params[:redirect_to].blank?
       "/auth/#{service}"
