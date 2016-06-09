73825590cba8e1c63f56426f0b848d7b55653b14
yuki_yano <yuki_yano@dwango.co.jp>
2016-06-09 15:02:13 +0900

    バッチのメソッド記述をシンプルに修正

--- lib/batch/twitter2slack.rb
+++ lib/batch/twitter2slack.rb
@@ -1,23 +1,21 @@
 module Batch
   class Twitter2slack
-    class << self
-      def exec
-        Twitter2slackCondition.all.map do |condition|
-          unsent_tweets = TwitterApiRepository.find_unsent_tweet(condition)
+    def self.exec
+      Twitter2slackCondition.all.map do |condition|
+        unsent_tweets = TwitterApiRepository.find_unsent_tweet(condition)
+        unsent_tweets.map do |tweet|
+          SlackApiRepository.post_message(condition.slack_channel, tweet[:url], 'twitter', icon_emoji: ':twitter:')
+        end
+
+        if condition.quote
           unsent_tweets.map do |tweet|
-            SlackApiRepository.post_message(condition.slack_channel, tweet[:url], 'twitter', icon_emoji: ':twitter:')
+            TwitterInfrastructure::Tweet.exec(tweet[:url])
           end
+        end
 
-          if condition.quote
-            unsent_tweets.map do |tweet|
-              TwitterInfrastructure::Tweet.exec(tweet[:url])
-            end
-          end
-
-          unless unsent_tweets.last.nil?
-            condition.last_tweet = unsent_tweets.last[:id]
-            condition.save
-          end
+        unless unsent_tweets.last.nil?
+          condition.last_tweet = unsent_tweets.last[:id]
+          condition.save
         end
       end
     end
--- lib/batch/update_lives.rb
+++ lib/batch/update_lives.rb
@@ -1,13 +1,11 @@
 module Batch
   class UpdateLives
-    class << self
-      def exec
-        lives = LiveApiRepository.find_nicovideo_live.select(&:validate)
-        lives.map(&:save)
+    def self.exec
+      lives = LiveApiRepository.find_nicovideo_live.select(&:validate)
+      lives.map(&:save)
 
-        Live.all.map do |live|
-          live.destroy if live.start_time + 30.minutes < Time.zone.now.to_datetime
-        end
+      Live.all.map do |live|
+        live.destroy if live.start_time + 30.minutes < Time.zone.now.to_datetime
       end
     end
   end
