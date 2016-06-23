f68730bba35029dfaebb1e9d3a0d768d580b4a9d
yuki_yano <yuki_yano@dwango.co.jp>
2016-06-23 18:34:57 +0900

    n+1問題を解決

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -7,9 +7,9 @@ module Api
 
     def show
       channel = SlackChannel.find(params[:id])
-      SlackApiRepository.find_all_messages_by_channel(channel, cache: false).select(&:validate).map(&:save)
-      SlackApiRepository.find_all_deleted_messages(channel, cache: true).map(&:destroy)
-      messages = channel.slack_message.limit(300).map(&:decorate)
+      # SlackApiRepository.find_all_messages_by_channel(channel, cache: false).select(&:validate).map(&:save)
+      # SlackApiRepository.find_all_deleted_messages(channel, cache: true).map(&:destroy)
+      messages = channel.slack_message.includes(:slack_channel).limit(300).map(&:decorate)
       render json: messages, each_serializer: MessageApiSerializer, root: nil
     end
   end
