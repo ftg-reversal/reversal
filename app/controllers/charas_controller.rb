dc292bc623ce18647de30334c67bb972e78610bb
tigberd <tigberd@gmail.com>
2016-07-01 02:33:44 +0900

    各ページにタイトルを追加

--- app/controllers/charas_controller.rb
+++ app/controllers/charas_controller.rb
@@ -1,11 +1,11 @@
 class CharasController < ApplicationController
   def video
     @charas = Chara.all
-    chara = Chara.find(params[:chara_id])
+    @chara = Chara.find(params[:chara_id])
     @videos = Video.includes(video_matchups: [:chara1, :chara2])
-      .where(video_matchups: { chara1: chara }).references(:video_matchups).order('posted_at DESC')
+      .where(video_matchups: { chara1: @chara }).references(:video_matchups).order('posted_at DESC')
       .or(Video.includes(video_matchups: [:chara1, :chara2])
-      .where(video_matchups: { chara2: chara }).references(:video_matchups).order('posted_at DESC'))
+      .where(video_matchups: { chara2: @chara }).references(:video_matchups).order('posted_at DESC'))
       .page(params[:page])
 
     render template: 'videos/index'
