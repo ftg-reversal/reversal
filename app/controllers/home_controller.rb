a407d712a216318c6d4c4c7ab257b969da7485f9
yuki_yano <yuki_yano@dwango.co.jp>
2016-07-11 13:29:59 +0900

    RlogのActivityが表示されていなかったのを修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
     activities = PublicActivity::Activity.includes(:owner)
-                                          .includes(:trackable)
-                                          .includes(:recipient)
-                                          .order('updated_at DESC')
-                                          .limit(500)
+                                         .includes(:trackable)
+                                         .includes(:recipient)
+                                         .order('updated_at DESC')
+                                         .limit(500)
 
     activities = activities.select { |activity| !activity.trackable.nil? }.first(60)
     @activities = Kaminari.paginate_array(activities).page(params[:page])
