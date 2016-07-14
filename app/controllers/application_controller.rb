4a090d60966acedc3fbc6f6d58a00f157aa1e8f7
tigberd <tigberd@gmail.com>
2016-07-14 21:04:21 +0900

    試しにレイアウト調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   serialization_scope :view_context
 
-  before_action :do_login
+  before_action :recent_activity, :do_login
+
+  def recent_activity
+    activities = PublicActivity::Activity.includes(:owner)
+                                         .includes(:trackable)
+                                         .includes(:recipient)
+                                         .order('updated_at DESC')
+                                         .limit(100)
+
+    @r_activities = activities.select { |activity| !activity.trackable.nil? }.first(10)
+  end
 
   def do_login
     login if session[:user_id]
