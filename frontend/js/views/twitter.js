731191df9dac65c07edaa53634e138014949e5d7
tigberd <tigberd@gmail.com>
2016-07-16 08:46:10 +0900

    ウィジェットを動的に生成するように修正

--- frontend/js/views/twitter.js
+++ frontend/js/views/twitter.js
@@ -1,5 +1,11 @@
 function onLoad() {
-  if (twttr) twttr.widgets.load();
+  if (twttr) {
+    twttr.widgets.load();
+
+    const widget = $('#online-bot-widget');
+    widget.empty();
+    twttr.widgets.createTimeline('753550279807672320', document.getElementById('online-bot-widget'));
+  }
 }
 
 window.addEventListener('turbolinks:load', onLoad);
