2a638cdd38b95b84b68fa51aa473bdf1446efda8
tigberd <tigberd@gmail.com>
2016-07-15 19:58:16 +0900

    リファクタリング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   serialization_scope :view_context
 
-  # before_action :recent_activity
   before_action :do_login
-
-  # def recent_activity
-  #   activities = PublicActivity::Activity.includes(:owner)
-  #                                        .includes(:trackable)
-  #                                        .includes(:recipient)
-  #                                        .order('updated_at DESC')
-  #                                        .limit(100)
-
-  #   @r_activities = activities.select { |activity| !activity.trackable.nil? }.first(10)
-  # end
 
   def do_login
     login if session[:user_id]
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
 class HomeController < ApplicationController
   def index
-    activities = PublicActivity::Activity.includes(:owner)
-                                         .includes(:trackable)
-                                         .includes(:recipient)
-                                         .order('updated_at DESC')
-                                         .limit(500)
+    activities = Activity.including_all.recently.limit(500)
 
     activities = activities.select { |activity| !activity.trackable.nil? }.first(60)
     @activities = Kaminari.paginate_array(activities).page(params[:page])
