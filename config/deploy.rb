56233eeb50c26b5832b94cddde1dfc4517881a1a
tigberd <tigberd@gmail.com>
2016-07-18 16:47:17 +0900

    rubyのバージョン修正漏れ

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:ftg-reversal/reversal.git'
 set :branch, fetch(:branch, 'master')
 
 set :rbenv_type, :user # :system or :user
-set :rbenv_ruby, '2.3.0'
+set :rbenv_ruby, '2.3.1'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w(rake gem bundle ruby rails)
 set :rbenv_roles, :all # default value
