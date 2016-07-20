6c61f5d0a0c844240e10587a3e763b9aa6a88627
tigberd <tigberd@gmail.com>
2016-07-21 05:25:46 +0900

    typoを修正

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,4 @@
-const gulp = require('gulp')
+const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const sorting = require('postcss-sorting');
 const stylefmt = require('gulp-stylefmt');
@@ -7,6 +7,6 @@ gulp.task('stylefmt', function () {
     return gulp.src('./frontend/css/**/*.css')
         .pipe(postcss([sorting({'sort-order': 'zen'})]))
         .pipe(stylefmt())
-        .pipe( gulp.dest('./frontend/css')
+        .pipe(gulp.dest('./frontend/css')
     );
 });
