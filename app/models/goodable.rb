3b89dccf21c92179550704e0a43ef7d8da75f452
tigberd <tigberd@gmail.com>
2016-07-23 14:54:13 +0900

    警告を修正

--- app/models/goodable.rb
+++ app/models/goodable.rb
@@ -1,13 +1,15 @@
 module Goodable
+  attr_accessor :goods_count
+
   def good?(_user)
     raise
   end
 
   def increment_counter
-    goods_count += 1
+    @goods_count += 1
   end
 
   def decrement_counter
-    goods_count -= 1
+    @goods_count -= 1
   end
 end
