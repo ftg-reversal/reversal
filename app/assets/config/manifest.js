7e43c5995a5031f60c8d1f1ee53ad9ee73847674
tigberd <tigberd@gmail.com>
2016-08-13 20:42:21 +0900

    ckeditorをasset precompileの対象に追加

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,5 +3,3 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
-
-//= link_tree ../javascripts/ckeditor
--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,6 +1,8 @@
 # Use this hook to configure ckeditor
 Ckeditor.setup do |config|
-  require "ckeditor/orm/active_record"
-  config.assets_languages = ['ja', 'en']
-  config.assets_plugins = ['autoembed', 'autolink', 'embed', 'embedbase', 'image2', 'imageresponsive', 'lineutils', 'notification', 'notificationaggregator', 'simpleuploads', 'widget']
+  require 'ckeditor/orm/active_record'
+  config.assets_languages = %w(ja en)
+  config.assets_plugins = %w(autoembed autolink embed embedbase image2 imageresponsive lineutils notification notificationaggregator simpleuploads widget)
 end
+
+Rails.application.config.assets.precompile += %w(ckeditor/*)
