c1bee47dca649d498e017049cc310cac184c7f11
tigberd <tigberd@gmail.com>
2016-08-19 17:28:37 +0900

    testを整理

--- spec/infrastructures/nicovideo_infrastructure/video_thumbnail_spec.rb
+++ spec/infrastructures/nicovideo_infrastructure/video_thumbnail_spec.rb
@@ -1,28 +1,17 @@
 require 'rails_helper'
-require 'webmock/rspec'
 
 module NicovideoInfrastructure
   describe VideoThumbnail do
-    describe '.exec' do
-      context 'sm***が渡されたとき' do
-        it '正しいサムネイルURLが取得出来ること' do
-          expect(VideoThumbnail.exec('http://www.nicovideo.jp/watch/sm12345')).to(
-            eq 'http://tn-skr2.smilevideo.jp/smile?i=12345')
-        end
-      end
+    it 'sm***が渡されたとき正しいサムネイルURLが取得出来ること' do
+      expect(VideoThumbnail.exec('http://www.nicovideo.jp/watch/sm12345')).to(eq 'http://tn-skr2.smilevideo.jp/smile?i=12345')
+    end
 
-      context 'nm***が渡されたとき' do
-        it '正しいサムネイルURLが取得出来ること' do
-          expect(VideoThumbnail.exec('http://www.nicovideo.jp/watch/nm12345')).to(
-            eq 'http://tn-skr2.smilevideo.jp/smile?i=12345')
-        end
-      end
+    it 'nm***が渡されたとき正しいサムネイルURLが取得出来ること' do
+      expect(VideoThumbnail.exec('http://www.nicovideo.jp/watch/nm12345')).to(eq 'http://tn-skr2.smilevideo.jp/smile?i=12345')
+    end
 
-      context 'それ以外のURLが渡されたとき' do
-        it 'nilが返ってくること' do
-          expect(VideoThumbnail.exec('http://www.nicovideo.jp/watch/12345')).to be_nil
-        end
-      end
+    it 'それ以外のURLが渡されたときnilが返ってくること' do
+      expect(VideoThumbnail.exec('http://www.nicovideo.jp/watch/12345')).to be_nil
     end
   end
 end
