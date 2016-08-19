38c88c6efec9d05cac48779c46900582f0408102
tigberd <tigberd@gmail.com>
2016-08-19 18:49:57 +0900

    一旦Sentryをコメントアウト

--- frontend/js/views/raven.js
+++ frontend/js/views/raven.js
@@ -1,1 +1,1 @@
-Raven.config('https://ae041dcf7d244acbacd469d1da735145@app.getsentry.com/93051').install()
+// Raven.config('https://ae041dcf7d244acbacd469d1da735145@app.getsentry.com/93051').install()
