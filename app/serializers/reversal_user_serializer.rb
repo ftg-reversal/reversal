ca8b1c31117ddb31b04086eb3280dc684dd5120d
tigberd <tigberd@gmail.com>
2016-08-24 14:43:10 +0900

    serializerにannotateを追加

--- app/serializers/reversal_user_serializer.rb
+++ app/serializers/reversal_user_serializer.rb
@@ -1,3 +1,29 @@
+# == Schema Information
+#
+# Table name: reversal_users
+#
+#  id              :integer          not null, primary key
+#  twitter_user_id :integer
+#  slack_user_id   :integer
+#  is_admin        :boolean          default(FALSE)
+#  screen_name     :string(255)
+#  name            :string(255)
+#  chara_id        :integer
+#  rank_id         :integer
+#  home            :text(65535)
+#  bio             :text(65535)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_reversal_users_on_chara_id         (chara_id)
+#  index_reversal_users_on_rank_id          (rank_id)
+#  index_reversal_users_on_screen_name      (screen_name)
+#  index_reversal_users_on_twitter_user_id  (twitter_user_id) UNIQUE
+#  slack_user_id                            (slack_user_id) UNIQUE
+#
+
 class ReversalUserSerializer < ActiveModel::Serializer
   attributes :id, :screen_name, :name
 end
--- app/serializers/slack_message_serializer.rb
+++ app/serializers/slack_message_serializer.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: slack_messages
+#
+#  id               :integer          not null, primary key
+#  slack_channel_id :integer          default(0), not null
+#  slack_user_id    :integer
+#  username         :string(255)
+#  icon             :text(65535)
+#  ts               :decimal(16, 6)
+#  text             :text(65535)
+#  attachments      :text(65535)
+#  file             :text(65535)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  channel_ts_index       (slack_channel_id,ts)
+#  channel_user_ts_index  (slack_channel_id,slack_user_id,ts) UNIQUE
+#  slack_channel_id       (slack_channel_id)
+#  slack_user_id          (slack_user_id)
+#  ts                     (ts)
+#
+
 class SlackMessageSerializer < ActiveModel::Serializer
   attributes :id, :slack_user, :username, :icon_url, :channel, :date, :format_text, :ts, :image, :attachments
   delegate :username, to: :object
