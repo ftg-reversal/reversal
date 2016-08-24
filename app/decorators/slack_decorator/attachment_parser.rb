6e43b774686e96645cdf9e3cbd5ba7c608f4fb09
tigberd <tigberd@gmail.com>
2016-08-24 15:46:56 +0900

    Revert "classのネストをリファクタリング"
    This reverts commit 9b1bf9e51945e5a8de73a0399fd4a7bfe27bddbf.

--- app/decorators/slack_decorator/attachment_parser.rb
+++ app/decorators/slack_decorator/attachment_parser.rb
@@ -1,26 +1,28 @@
-class SlackDecorator::AttachmentParser
-  class << self
-    def exec(at)
-      if at['service_name'] == 'twitter'
-        twitter(at)
-      else
-        others(at)
+module SlackDecorator
+  class AttachmentParser
+    class << self
+      def exec(at)
+        if at['service_name'] == 'twitter'
+          twitter(at)
+        else
+          others(at)
+        end
       end
-    end
 
-    private
+      private
 
-    def twitter(at)
-      at
-    end
-
-    def others(at)
-      if at[:title] || at[:text]
-        at[:title_link] = "https://slack-redir.net/link?url=#{URI.encode at[:title_link]}" if at[:title_link]
+      def twitter(at)
         at
-      elsif at[:image_url]
-        at[:service_name] = 'reversal-image'
-        at
+      end
+
+      def others(at)
+        if at[:title] || at[:text]
+          at[:title_link] = "https://slack-redir.net/link?url=#{URI.encode at[:title_link]}" if at[:title_link]
+          at
+        elsif at[:image_url]
+          at[:service_name] = 'reversal-image'
+          at
+        end
       end
     end
   end
