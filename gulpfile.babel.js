3d4b7b214c5385fe236c72054d875b234b63fb62
tigberd <tigberd@gmail.com>
2016-08-24 12:49:47 +0900

    文法を修正

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,15 @@
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
-const postcssReporter = require('postcss-reporter')
+const postcssReporter = require('postcss-reporter');
 const sorting = require('postcss-sorting');
 const stylefmt = require('gulp-stylefmt');
-const stylelint = require('stylelint')
+const stylelint = require('stylelint');
 
 gulp.task('stylefmt', function () {
     return gulp.src('./frontend/css/**/*.css')
         .pipe(postcss([sorting({'sort-order': 'zen'})]))
         .pipe(stylefmt())
-        .pipe(gulp.dest('./frontend/css')
-    );
+        .pipe(gulp.dest('./frontend/css'));
 });
 
 gulp.task('stylelint', function () {
