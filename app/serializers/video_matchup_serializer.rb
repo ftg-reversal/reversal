2d12225189041cb2556dcd9432ca8aefb4ed7a64
tigberd <tigberd@gmail.com>
2016-08-28 22:02:42 +0900

    annotateを追加

--- app/serializers/video_matchup_serializer.rb
+++ app/serializers/video_matchup_serializer.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: video_matchups
+#
+#  id        :integer          not null, primary key
+#  video_id  :integer          not null
+#  chara1_id :integer          not null
+#  chara2_id :integer          not null
+#  sec       :integer          not null
+#
+# Indexes
+#
+#  index_video_matchups_on_chara1_id  (chara1_id)
+#  index_video_matchups_on_chara2_id  (chara2_id)
+#  index_video_matchups_on_video_id   (video_id)
+#
+
 class VideoMatchupSerializer < ActiveModel::Serializer
   attributes :id, :video_id, :chara1_id, :chara2_id, :sec
 end
