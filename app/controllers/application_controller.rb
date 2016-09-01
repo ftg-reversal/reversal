d66cc139f5391b72426ff36c987383b5c33aecdf
tigberd <tigberd@gmail.com>
2016-09-01 20:18:08 +0900

    動画一覧ページを英語に対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   serialization_scope :view_context
 
+  before_action :set_locale
   before_action :initialize_search_query
   before_action :do_login
 
@@ -13,11 +14,21 @@ class ApplicationController < ActionController::Base
     redirect_to '/' unless @current_user
   end
 
-  private
+  def set_locale
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    I18n.locale = extract_locale_from_accept_language_header
+    logger.debug "* Locale set to '#{I18n.locale}'"
+  end
 
   def initialize_search_query
     @q = ''
     @q_type = ''
+  end
+
+  private
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
 
   def login
--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,7 @@ module Reversal
     config.generators.fixture_replacement :factory_girl, dir: 'spec/factory_girl'
 
     config.i18n.default_locale = :ja
+    config.i18n.available_locales = [:ja, :en]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
