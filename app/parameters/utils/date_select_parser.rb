1b87ca94f40c302286840b441f670d014f90a75b
tigberd <tigberd@gmail.com>
2016-09-05 21:30:15 +0900

    DateSelectParserが不要なので削除

--- app/parameters/utils/date_select_parser.rb
+++
@@ -1,9 +0,0 @@
-module Utils
-  class DateSelectParser
-    def self.exec(params, key)
-      date_parts = params.select { |k, _| k.to_s =~ /\A#{key}\([1-6]{1}i\)/ }.values
-      date_str = date_parts[0..2].join('-') + ' ' + date_parts[3..-1].join(':')
-      Time.zone.parse(date_str).to_datetime
-    end
-  end
-end
