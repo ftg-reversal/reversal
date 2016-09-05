156df25e99f2cdcf03e9e73dcfd6dc02928f54a5
tigberd <tigberd@gmail.com>
2016-09-05 21:13:59 +0900

    localeの検出例外時にenに設定するように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,9 +15,7 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
     I18n.locale = extract_locale_from_accept_language_header
-    logger.debug "* Locale set to '#{I18n.locale}'"
   end
 
   def initialize_search_query
@@ -29,6 +27,8 @@ class ApplicationController < ActionController::Base
 
   def extract_locale_from_accept_language_header
     request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  rescue
+    'en'
   end
 
   def login
