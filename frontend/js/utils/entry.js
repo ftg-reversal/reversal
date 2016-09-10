b247737b40014caa8b9488793743b6a6f34a0cf6
tigberd <tigberd@gmail.com>
2016-09-10 23:47:24 +0900

    eslintに沿って修正

--- frontend/js/utils/entry.js
+++ frontend/js/utils/entry.js
@@ -1,26 +1,28 @@
-require('vendor/jquery.validationEngine-ja.js');
-require('vendor/jquery.validationEngine');
+import $ from 'jquery';
+import salert from 'sweetalert';
+import '../vendor/jquery.validationEngine-ja';
+import '../vendor/jquery.validationEngine';
 
 function onLoad() {
   $('#new-entry-submit').click((e) => {
     e.preventDefault();
 
-    if (!$("#new_entry").validationEngine('validate')) {
+    if (!$('#new_entry').validationEngine('validate')) {
       return false;
     }
 
-    const eventTitle = $('h2:first').text().replace(/\s+/g, "");
+    const eventTitle = $('h2:first').text().replace(/\s+/g, '');
 
     const players = {
       names: [],
       charas: [],
-      ranks: []
-    }
+      ranks: [],
+    };
+
     $('div[id ^= player]').each((i) => {
-      i++;
-      players.names.push($(`#player_name_${i}`).val());
-      players.charas.push($(`#player_chara_${i}`).val());
-      players.ranks.push($(`#player_rank_${i}`).val());
+      players.names.push($(`#player_name_${i + 1}`).val());
+      players.charas.push($(`#player_chara_${i + 1}`).val());
+      players.ranks.push($(`#player_rank_${i + 1}`).val());
     });
 
     $.ajax({
@@ -28,25 +30,26 @@ function onLoad() {
       url: '/entries',
       data: Object.assign(players, {
         authenticity_token: $('meta[name="csrf-token"]').attr('content'),
-        [$('#entry_event').attr('name')] : $('#entry_event').attr('value'),
-        [$('#entry_comment').attr('name')] : $('#entry_comment').val()
-      })
-    }).done((data) => {
+        [$('#entry_event').attr('name')]: $('#entry_event').attr('value'),
+        [$('#entry_comment').attr('name')]: $('#entry_comment').val(),
+      }),
+    }).done((data) => { // eslint-disable-line no-unused-vars
       $('#entryModal').modal('hide');
-      swal({
+      salert({
         title: 'エントリー完了',
-        text: 'エントリーに成功しました<br>エントリーしたことをツイートする<br><a href="https://twitter.com/intent/tweet?button_hashtag=ftg_reversal&text=' + eventTitle + 'にエントリーしました" class="twitter-hashtag-button" data-size="large" data-url="' + window.location.href + '">',
-        type:  'success',
-        html: true
-      }, () => { location.reload() });
-      twttr.widgets.load();
-    }).fail((data) => {
-      swal({
+        text: `エントリーに成功しました<br>エントリーしたことをツイートする<br><a href="https://twitter.com/intent/tweet?button_hashtag=ftg_reversal&text=${eventTitle}にエントリーしました" class="twitter-hashtag-button" data-size="large" data-url="${window.location.href}">`,
+        type: 'success',
+        html: true,
+      }, () => { location.reload(); });
+      twttr.widgets.load();// eslint-disable-line no-undef
+    }).fail((data) => { // eslint-disable-line no-unused-vars
+      salert({
         title: 'エントリー失敗',
         text: '入力を確認してください',
-        type:  'error'
+        type: 'error',
       });
     });
+    return true;
   });
 }
 
