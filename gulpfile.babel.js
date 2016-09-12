c630c94c3b2a296df06aa33a9f8b0ad1287d3ca9
tigberd <tigberd@gmail.com>
2016-09-12 16:02:52 +0900

    lintのタスクを追加

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,17 @@
 import gulp from 'gulp';
+import eslint from 'gulp-eslint';
 import postcss from 'gulp-postcss';
 import postcssReporter from 'postcss-reporter';
 import sorting from 'postcss-sorting';
 import stylefmt from 'gulp-stylefmt';
 import stylelint from 'stylelint';
+
+gulp.task('lint', () => (
+  gulp.src(['frontend/js/**/*.js', 'frontend/js/**/*.jsx', '!frontend/js/vendor/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+));
 
 gulp.task('stylefmt', () => (
   gulp.src('./frontend/css/**/*.css')
