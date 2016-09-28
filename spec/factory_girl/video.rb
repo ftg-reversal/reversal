b764152894f35f2bb877f081cde486ef70678b96
tigberd <tigberd@gmail.com>
2016-09-28 16:50:55 +0900

    rubocopに沿って修正

--- spec/factory_girl/video.rb
+++ spec/factory_girl/video.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
     sequence(:url) { |n| "url#{n}" }
     video_site VideoSite::Dummy.new
     sequence(:title) { |n| "title#{n}" }
-    posted_at Time.parse('2015-01-01 00:00:00 +0900')
+    posted_at Time.zone.parse('2015-01-01 00:00:00 +0900')
   end
 end
