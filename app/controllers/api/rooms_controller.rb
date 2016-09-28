34d54504972ba49f5426430173e28095ba5b9c54
tigberd <tigberd@gmail.com>
2016-09-29 03:29:52 +0900

    room関連のものを削除

--- app/controllers/api/rooms_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::RoomsController < ApplicationController
-  def index
-    render json: Room.all
-  end
-
-  def show
-    render json: Message.where(room: @room).page(params[:page])
-  end
-end
