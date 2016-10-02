cf653cd660f43fedbcacf13cd8b1e8143b1cc4d1
tigberd <tigberd@gmail.com>
2016-10-02 18:45:37 +0900

    日付について追加

--- app/script/mikado23on_aggregate_script.rb
+++ app/script/mikado23on_aggregate_script.rb
@@ -4,14 +4,20 @@ class Mikado23onAggregateScript
 
     Mikado23onVote.all.select { |vote| vote.reversal_user.twitter_user }.each do |vote|
       print "#{vote.reversal_user.twitter_user.name},"
+      print "#{vote.reversal_user.twitter_user.screen_name},"
+      print "#{vote.area},"
 
       charas.each do |chara|
         entry_num = vote[chara]
-        player = Mikado23onEntry.find_by(chara: Chara.find_by(en_name: chara), entry_num: entry_num)&.name
+        player = Mikado23onEntry.find_by(area: vote.area, chara: Chara.find_by(en_name: chara), entry_num: entry_num)&.name
         player = player ? player : '投票なし'
         print "#{player},"
       end
-      puts ''
+      if vote.created_at
+        puts vote.created_at
+      else
+        puts ''
+      end
     end
   end
 end
